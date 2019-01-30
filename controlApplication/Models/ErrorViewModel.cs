1cd3cf7745fefb56b4229a646aa79f0a12947959
Chernetsov Vladimir <chernetsov@graviongroup.ru>
2019-01-30 17:33:49 +0300

    Add project files.

---
+++ controlApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace controlApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
